9a9c0c45d370e4f330fb92a7eb6110666fa59347
SoleilUmwiza92 <soleilmwiza@gmail.com>
2023-05-13 23:40:42 +0200

    Removing unnecessary spaces

--- src/components/nav-bar-buttons.vue
+++ src/components/nav-bar-buttons.vue
@@ -2,7 +2,6 @@
   <div class="nav-bar__buttons">
     <template v-if="!isAuthenticated">
       <LoginButton /> <br><br>
-
       <SignupButton /> <br>
     </template>
     <template v-if="isAuthenticated">
